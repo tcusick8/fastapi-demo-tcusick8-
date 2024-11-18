6a8e23853ac51ac9d66801b416d10a0c8cc45fd6
Thomas Cusick <thomascusick@Thomass-MacBook-Air-2.local>
2024-11-18 18:30:51 -0500

    updated fast-api dir

--- app/main.py
+++ app/main.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python3
 
-from fastapi import FastAPI
+from fastapi import Request, FastAPI
 from typing import Optional
 from pydantic import BaseModel
+import pandas as pd
 import json
 import os
 import math
-
+import mysql.connector
+import uvicorn
+from mysql.connector import Error
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+if __name__ == "__main__":
+   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
 
 @app.get("/")  # zone apex
 def zone_apex():
@@ -39,3 +58,85 @@ def sqrt(h: int):
 @app.get("/power/{base}/{exponent}")
 def power(base: int, exponent: int):
     return {"result": math.pow(base, exponent)}
+
+@app.get("/customer/{idx}")
+def customer(idx: int):
+    #read the data into a df
+    df = pd.read_csv("../customers.csv")
+    #filter the data based on index
+    customer = df.iloc[idx]
+    return customer.to_dict()
+
+@app.post("/get_body")
+async def get_body(request: Request):
+        return await request.json()
+
+
+
+
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "tpg6hu"
+
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
+
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+
+
+@app.get('/songs')
+def get_songs():
+    query = """
+    SELECT 
+        songs.title AS title,
+        songs.album AS album,
+        songs.artist AS artist,
+        songs.year AS year,
+        CONCAT('https://your-s3-bucket.s3.amazonaws.com/', songs.file) AS file,
+        CONCAT('https://your-s3-bucket.s3.amazonaws.com/', songs.image) AS image,
+        genres.genre AS genre
+    FROM 
+        songs
+    JOIN 
+        genres ON songs.genre = genres.genreid
+    ORDER BY 
+        songs.title;
+    """
+    try:
+        cur.execute(query)
+        headers = [x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data = []
+        for result in results:
+            json_data.append(dict(zip(headers, result)))
+        return json_data
+    except mysql.connector.Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
